7d9a9732d76bc574b1c897a60b201cbe339eb737
Roman Cebula <ceboolion@gmail.com>
2020-05-20 22:53:52 +0200

    feature/ MapKit, Addition of annotations, addition of navigation item to choose map's style

---
+++ Project16/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Ceboolion on 20/05/2020.
+//  Copyright © 2020 Ceboolion. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        capitals()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Map Style", style: .plain, target: self, action: #selector(changeMapStyle))
+    }
+    
+    @objc func changeMapStyle(){
+        let ac = UIAlertController(title: "Choose map style", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Satellite", style: .default, handler: { action in
+            self.mapView.mapType = MKMapType.satellite
+        }))
+        ac.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: { action in
+            self.mapView.mapType = MKMapType.hybrid
+        }))
+        ac.addAction(UIAlertAction(title: "Standard", style: .default, handler: { action in
+            self.mapView.mapType = MKMapType.standard
+        }))
+        present(ac, animated: true)
+    }
+    
+    func capitals(){
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 summer olimpics.")
+        let oslo  = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.74), info: "It was founded 1000 years ago.")
+        let paris  = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Ofen called the city of light")
+        let rome  = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a hole country inside it")
+        let washington  = Capital(title: "Washington", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself")
+        
+        mapView.addAnnotations([london,oslo,paris,rome,washington])
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Capital else {return nil}
+        let identifier = " Capital"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            annotationView?.pinTintColor = .magenta
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = btn
+        } else {
+            annotationView?.annotation = annotation
+        }
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+    }
+}
